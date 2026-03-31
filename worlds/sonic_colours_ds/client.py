4094800ae10ebdb894a1b643d9f51e7654fbc5fc
SilicDev <83308290+SilicDev@users.noreply.github.com>
2026-03-31 16:04:33 +0200

    Bizhawk 2.10 required
    versions <2.10 do not allow reading the ROM directly

--- worlds/sonic_colours_ds/client.py
+++ worlds/sonic_colours_ds/client.py
@@ -1,6 +1,7 @@
 
 from typing import TYPE_CHECKING
 
+import worlds._bizhawk as bizhawk
 from worlds._bizhawk.client import BizHawkClient
 
 if TYPE_CHECKING:
@@ -12,8 +13,23 @@ class SonicColoursDSClient(BizHawkClient):
     system = "NDS"
     patch_suffix = ".apscds"
 
+    def initialize_client(self) -> None:
+        pass
+
     async def validate_rom(self, ctx: "BizHawkClientContext") -> bool:
-        #from CommonClient import logger
+        from CommonClient import logger
+
+        try:
+            # Check ROM name
+            rom_name_bytes = ((await bizhawk.read(ctx.bizhawk_ctx, [(0x0, 16, "ROM")]))[0])
+            rom_name = bytes([byte for byte in rom_name_bytes if byte != 0]).decode("ascii")
+            if not rom_name == "SONICCOLORSBXSP":
+                return False
+        except UnicodeDecodeError:
+            return False
+        except bizhawk.RequestFailedError:
+            return False  # Should verify on the next pass
+
         ctx.game = self.game
         ctx.items_handling = 0b001
         ctx.want_slot_data = True
