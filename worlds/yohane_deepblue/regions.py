471b34bcc8785e08187c1134243c82e20c2cbb8e
SilicDev <83308290+SilicDev@users.noreply.github.com>
2026-04-11 02:56:33 +0200

    Basic Regions

--- worlds/yohane_deepblue/regions.py
+++ worlds/yohane_deepblue/regions.py
@@ -6,13 +6,72 @@ from .locations import *
 from .data import LocationNames, ItemNames
 
 def create_regions(world: World, active_locations: dict[str, int]) -> None:
-    menu_region = create_region(world, "Menu", active_locations, location_table.keys())
-    
+    menu_region = create_region(world, "Menu", active_locations, set(location_table.keys()))
+    sunken_temple_region = create_region(world, LocationNames.sunken_temple_region, active_locations, set())
+    ruins_region = create_region(world, LocationNames.ruins_region, active_locations, set())
+    grotto_region = create_region(world, LocationNames.grotto_region, active_locations, set())
+    coral_hill_region = create_region(world, LocationNames.coral_hill_region, active_locations, set())
+    sea_of_trees_region = create_region(world, LocationNames.sea_of_trees_region, active_locations, set())
+    crystalline_grotto_region = create_region(world, LocationNames.crystalline_grotto_region, active_locations, set())
+    sunken_volcano_left_region = create_region(world, LocationNames.sunken_volcano_left_region, active_locations, set())
+    sunken_volcano_right_region = create_region(world, LocationNames.sunken_volcano_right_region, active_locations, set())
+    shipwreck_region = create_region(world, LocationNames.shipwreck_region, active_locations, set())
+    infernal_altar_region = create_region(world, LocationNames.infernal_altar_region, active_locations, set())
+    aqours_memoria_region = create_region(world, LocationNames.aqours_memoria_region, active_locations, set())
+
     world.multiworld.regions += [
         menu_region,
+        sunken_temple_region,
+        ruins_region,
+        grotto_region,
+        coral_hill_region,
+        sea_of_trees_region,
+        crystalline_grotto_region,
+        sunken_volcano_left_region,
+        sunken_volcano_right_region,
+        shipwreck_region,
+        infernal_altar_region,
+        aqours_memoria_region,
     ]
 
 def connect_regions(world: World) -> None:
+    connect(world, "Menu", LocationNames.sunken_temple_region, None)
+    connect(world, LocationNames.grotto_region, LocationNames.ruins_region, None)
+    connect(world, LocationNames.sunken_temple_region, LocationNames.grotto_region, 
+            lambda state: state.has(ItemNames.chika_unlock, world.player) or 
+                state.has_all([
+                    ItemNames.ruby_unlock,
+                    ItemNames.ruby_upgrade
+                ], world.player))
+    connect(world, LocationNames.grotto_region, LocationNames.coral_hill_region, 
+            lambda state: _coral_hill_access_rule(world, state))
+    connect(world, LocationNames.shipwreck_region, LocationNames.coral_hill_region,  
+            lambda state: _coral_hill_access_rule(world, state))
+    connect(world, LocationNames.shipwreck_region, LocationNames.sea_of_trees_region, 
+            lambda state: state.has(ItemNames.kanan_unlock, world.player) or 
+                state.has_all([
+                    ItemNames.mari_unlock,
+                    ItemNames.mari_upgrade,
+                    ItemNames.fallen_angels_soarshoes,
+                    ItemNames.gloves_of_might
+                ], world.player))
+    connect(world, LocationNames.coral_hill_region, LocationNames.crystalline_grotto_region, 
+            lambda state: state.has(ItemNames.gloves_of_might, world.player))
+    connect(world, LocationNames.ruins_region, LocationNames.sunken_volcano_left_region, None)
+    connect(world, LocationNames.sunken_volcano_left_region, LocationNames.sunken_volcano_right_region, 
+            lambda state: state.has(ItemNames.kanan_unlock, world.player))
+    connect(world, LocationNames.grotto_region, LocationNames.shipwreck_region, 
+            lambda state: state.has(ItemNames.sea_deitys_charm, world.player))
+    connect(world, LocationNames.coral_hill_region, LocationNames.shipwreck_region, 
+            lambda state: state.has(ItemNames.sea_deitys_charm, world.player))
+    connect(world, LocationNames.sea_of_trees_region, LocationNames.shipwreck_region, None)
+    connect(world, LocationNames.sunken_temple_region, LocationNames.infernal_altar_region, 
+            lambda state: state.has_all([
+                    ItemNames.riko_unlock,
+                    ItemNames.kanan_unlock,
+                    ItemNames.gloves_of_might
+                ], world.player))
+    connect(world, LocationNames.infernal_altar_region, LocationNames.aqours_memoria_region, None)
     pass
 
 def create_region(world: World, name: str, active_locations: dict[str, int], location_set: set[str]) -> Region:
@@ -34,3 +93,14 @@ def connect(world: World, source: str, destination: str, rule: typing.Optional[t
     
     source_region.exits.append(entrance)
     entrance.connect(dest_region)
+
+def _coral_hill_access_rule(world: World, state: CollectionState) -> bool:
+    return (state.has(ItemNames.gloves_of_might, world.player) and state.has_any([
+        ItemNames.you_unlock,
+        ItemNames.dia_unlock,
+        ItemNames.fallen_angels_soarshoes
+    ], world.player)) or (state.has(ItemNames.you_unlock, world.player) and state.has_any([
+        ItemNames.gloves_of_might,
+        ItemNames.dia_unlock,
+        ItemNames.fallen_angels_soarshoes
+    ], world.player))
